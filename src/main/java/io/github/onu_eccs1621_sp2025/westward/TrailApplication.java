6af2b21af0a91a424f202efdbd7bde8397017fea
d-catte <d-catte@onu.edu>
2026-02-17 20:51:26 -0500

    Bump version

--- src/main/java/io/github/onu_eccs1621_sp2025/westward/TrailApplication.java
+++ src/main/java/io/github/onu_eccs1621_sp2025/westward/TrailApplication.java
@@ -36,7 +36,7 @@ public class TrailApplication extends Application {
     /**
      * The build version of Westward
      */
-    private static final String VERSION = "v1.0.0-beta.1";
+    private static final String VERSION = "v1.0.0-beta.4";
     /**
      * The path to all local game data files
      */
